84de4f49e5723ac5b5af9393e722191ed517ff6a
源动力 <98251238+yuan-power-plus@users.noreply.github.com>
2024-01-22 01:07:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wxc837e854e8f77125',
+  APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '9b21d2742077cbaaede163e682119758',
+  APP_SECRET: '',
 
   PROVINCE: '河南',
   CITY: '信阳',
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '贾',
+      name: '',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ouTx26hJ5YeR1qEROwk5eK9ttYVc',
+      id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'QM4-HqqCWuLz6KCpQy4Oo1Bi3RDSa59bQTRjFN2KRMA',
+      useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-22',
       horoscopeDateType: '今日',
@@ -35,7 +35,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '贾', year: '2024', date: '01-22',
+          type: '生日', name: '', year: '2024', date: '01-22',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
@@ -53,13 +53,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'hkk6UNxqAQNxgdwiSxSjGRs3P-CKuZIq4c9S1qG2MMI',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ouTx26nZ9MGO02dPwbalbpiglvV4',
+      id: '',
     }
   ],
 
