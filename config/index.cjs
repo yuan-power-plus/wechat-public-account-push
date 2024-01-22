a9a7ba0281d8190d89ca28c25171bae02a7366c6
源动力 <98251238+yuan-power-plus@users.noreply.github.com>
2024-01-22 18:33:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,15 +25,30 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'D5iEwaBl_8n1d7fFZae32Q1Yove8NLArpXbEQoQrYiQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '01-22',
-      horoscopeDateType: '今日',
-      openUrl:'https://github.com/yuan-power-plus',
+      horoscopeDate: '12-27',
       festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '主人', year: '2024', date: '01-22',
+          type: '生日', name: '主人', year: '2005', date: '09-24',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
         },
       ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+  ],
+
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: 'YS4vAIxWDOX9D15_Fsr1N6OlmNhpT4aJGPDRowLW9vk',
@@ -49,4 +64,3 @@ const USER_CONFIG = {
 }
 
 module.exports = USER_CONFIG
-
