5b7f052800dceda2d1abeba32d7578417ce0c159
源动力 <98251238+yuan-power-plus@users.noreply.github.com>
2024-01-22 23:03:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,21 +1,31 @@
 /* eslint-disable */
+
+/**
+ * 此项目配置为方便新人使用，已缩减至最简配置。
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 自行添加属性，以支持更多个性化功能
+ */
 const USER_CONFIG = {
 
-  USE_PASSAGE: 'wechat-test',
-  
+  // 使用微信测试号：公众号APP_ID
   APP_ID: '',
+
+  // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
+
+  PROVINCE: '四川',
+  CITY: '南充',
 
   USERS: [
     {
+      // 想要发送的人的名字
       name: '主人',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ouTx26gxwDLnUE7ZxAttBmlArzaU',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'wwX3rrthN0IHXkST8ObXDXLjop6Bv7GyaHALTzP71bM',
-      province: '四川',
-      city: '南充',
-      horoscopeDate: '09-24',
-      horoscopeDateType: '今日',
-      openUrl: 'https://github.com/yuan-power-plus',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -39,26 +49,18 @@ const USER_CONFIG = {
     },
   ],
 
-      
-  /**
-   * 【推送完成提醒】 相关，主要用来展示发送是否成功/失败的数据
-   */
 
-  // 使用微信测试号：【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  // 使用其他通道时，请填写 config/template-config.cjs 中【推送完成提醒】模板的id
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: 'oES-nuJV2J7ml28haCZLXKBOodpXjZ74E8ESlONsykg',
 
-  // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
     {
       name: '自己',
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ouTx26nZ9MGO02dPwbalbpiglvV4',
     }
   ],
 
-
- 
-
+}
 
 module.exports = USER_CONFIG
-
