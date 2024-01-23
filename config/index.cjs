cd54d99a634ce664e7e4af88162b357b99a91e31
源动力 <98251238+yuan-power-plus@users.noreply.github.com>
2024-01-23 15:18:16 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,63 +1,55 @@
 /* eslint-disable */
+
+/**
+ * 此项目配置为方便新人使用，已缩减至最简配置。
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 自行添加属性，以支持更多个性化功能
+ */
 const USER_CONFIG = {
-  USE_PASSAGE: 'wechat-test',
+
+  // 使用微信测试号：公众号APP_ID
   APP_ID: '',
+
+  // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  
-  /**
-   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
-   */
+  PROVINCE: '四川',
+  CITY: '南充',
+
   USERS: [
     {
       // 想要发送的人的名字
       name: '主人',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      // 使用其他通道时，请严格按照各个通道的教程进行填写
       id: 'ouTx26gxwDLnUE7ZxAttBmlArzaU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
       useTemplateId: 'tGkgO5sGyrfsw7JslHpU9CSy90Qau5-qFSKQHGDBktM',
-      // 所在省份或城市，也可以不填
-      province: '四川',
-      // 所在城市或县区
-      city: '南充',
-    }
-    ],
-      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
           type: '*生日', name: '主人', year: '2005', date: '08-21',
         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '主人', year: '2005', date: '09-24',
-        }
       ],
-      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 相识的日子
-        { keyword: 'meet_day', date: '2023-09-01' }
+        { keyword: 'meet_day', date: '2023-09-01' },
       ],
+    },
+  ],
 
-      
-  /**
-   * 【推送完成提醒】 相关，主要用来展示发送是否成功/失败的数据
-   */
 
-  // 使用微信测试号：【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  // 使用其他通道时，请填写 config/template-config.cjs 中【推送完成提醒】模板的id
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: 'oES-nuJV2J7ml28haCZLXKBOodpXjZ74E8ESlONsykg',
 
-  // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
     {
-      // 一般都填自己
       name: '自己',
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ouTx26nZ9MGO02dPwbalbpiglvV4',
     }
   ],
 
-module.exports = USER_CONFIG
+}
 
+module.exports = USER_CONFIG
