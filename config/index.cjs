d6e25062008e1f2bfdebe0758b73ea8741d89fbe
源动力 <98251238+yuan-power-plus@users.noreply.github.com>
2024-01-23 15:11:33 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -3,48 +3,7 @@ const USER_CONFIG = {
   USE_PASSAGE: 'wechat-test',
   APP_ID: '',
   APP_SECRET: '',
-  MAX_PUSH_ONE_MINUTE: 5,
-  // 配合MAX_PUSH_ONE_MINUTE使用，休眠<SLEEP_TIME>毫秒后再发送剩余消息，不填则默认为65000
-  SLEEP_TIME: 65000,
 
-  SWITCH: {
-    /** 每日天气 */
-    weather: true,
-
-    /** 节假日 */
-    holidaytts: false,
-
-    /** 每日N句 */
-    CIBA: true,
-    
-    /*每日一言*/
-    oneTalk: false,
-    
-    /*土味情话(彩虹屁)*/
-    earthyLoveWords: true,
-    
-    /*朋友圈文案*/
-    momentCopyrighting: false,
-    
-    /*毒鸡汤*/
-    poisonChickenSoup: false,
-    
-    /*古诗古文*/
-    poetry: true,
-
-    /** 星座运势 */
-    horoscope: false,
-  
-    /** 生日消息和节日消息 */
-    birthdayMessage: true,
-  
-    /** 学生课表 */
-    courseSchedule: false,
-  },
-  
-  /** 是否给文字设置多彩颜色, 和emoji不兼容 */
-  // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
-  IS_SHOW_COLOR: false,
   
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
@@ -58,11 +17,11 @@ const USER_CONFIG = {
       id: 'ouTx26gxwDLnUE7ZxAttBmlArzaU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'tGkgO5sGyrfsw7JslHpU9CSy90Qau5-qFSKQHGDBktM	',
+      useTemplateId: 'tGkgO5sGyrfsw7JslHpU9CSy90Qau5-qFSKQHGDBktM',
       // 所在省份或城市，也可以不填
       province: '四川',
       // 所在城市或县区
-      city: '仪陇',
+      city: '南充',
     }
     ],
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
@@ -96,35 +55,9 @@ const USER_CONFIG = {
     {
       // 一般都填自己
       name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      // 使用其他通道时，请严格按照各个通道的教程进行填写
       id: 'ouTx26nZ9MGO02dPwbalbpiglvV4',
     }
   ],
-
-  /**
-   * 信息配置
-   */
-  /** 重要节日相关 */
-
-  /**
-   * 重要节日，修改名字为对应需要显示的名字, date 仅填月日即可, 请严格按照示例填写
-   * type必须填！ 只能 “生日” 和 “节日” 二选一!
-   *
-   * --- 阴历配置开始 ---
-   * 如果日期使用阴历，请在 “生日” 或 "节日“ 前添加 * 符号
-   * --- 阴历配置结束 ---
-   *
-   * 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
-   * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
-   *
-   * --- 是否展示周岁信息开始 ---
-   * isShowAge: true 展示岁数, 仅type为生日生效
-   * isShowAge: false 不展示岁数, 仅type为生日生效
-   * 删除isShowAge属性，也会不展示岁数
-   * --- 是否展示周岁信息结束 ---
-   */
-
 
 module.exports = USER_CONFIG
 
