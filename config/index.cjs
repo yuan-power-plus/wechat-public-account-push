2e82352c7937c25024890e026b5a4af0f945ccb7
源动力 <98251238+yuan-power-plus@users.noreply.github.com>
2024-01-23 15:43:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,7 +7,7 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '四川',
+  PROVINCE: '',
   CITY: '南充',
 
   USERS: [
@@ -29,7 +29,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '狼牙土豆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ouTx26nIl_cvO19rOiR24YpXb-Zw',
+      id: 'ouTx26nZ9MGO02dPwbalbpiglvV4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'pN_BIivx7zCy7koEVTxoAnxzPoahdBU8vn1DIZLW2Jg',
       festivals: [
