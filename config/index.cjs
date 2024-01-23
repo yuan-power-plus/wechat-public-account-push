8bf932792c0673b282b3d4343db04365662fc4b6
源动力 <98251238+yuan-power-plus@users.noreply.github.com>
2024-01-23 15:22:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,10 +1,4 @@
 /* eslint-disable */
-
-/**
- * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
- * 自行添加属性，以支持更多个性化功能
- */
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
@@ -24,6 +18,8 @@ const USER_CONFIG = {
       id: 'ouTx26gxwDLnUE7ZxAttBmlArzaU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'tGkgO5sGyrfsw7JslHpU9CSy90Qau5-qFSKQHGDBktM',
+    }
+    ],
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
