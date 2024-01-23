5617179ac9e25d26ccb7f0d29846733e6733f4b7
源动力 <98251238+yuan-power-plus@users.noreply.github.com>
2024-01-23 15:06:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,44 +1,130 @@
 /* eslint-disable */
-
-/**
- * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
- * 自行添加属性，以支持更多个性化功能
- */
 const USER_CONFIG = {
-
-  // 使用微信测试号：公众号APP_ID
+  USE_PASSAGE: 'wechat-test',
   APP_ID: '',
-
-  // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
+  MAX_PUSH_ONE_MINUTE: 5,
+  // 配合MAX_PUSH_ONE_MINUTE使用，休眠<SLEEP_TIME>毫秒后再发送剩余消息，不填则默认为65000
+  SLEEP_TIME: 65000,
 
-  PROVINCE: '四川',
-  CITY: '南充',
+  SWITCH: {
+    /** 每日天气 */
+    weather: true,
 
+    /** 节假日 */
+    holidaytts: false,
+
+    /** 每日N句 */
+    CIBA: true,
+    
+    /*每日一言*/
+    oneTalk: false,
+    
+    /*土味情话(彩虹屁)*/
+    earthyLoveWords: true,
+    
+    /*朋友圈文案*/
+    momentCopyrighting: false,
+    
+    /*毒鸡汤*/
+    poisonChickenSoup: false,
+    
+    /*古诗古文*/
+    poetry: true,
+
+    /** 星座运势 */
+    horoscope: false,
+  
+    /** 生日消息和节日消息 */
+    birthdayMessage: true,
+  
+    /** 学生课表 */
+    courseSchedule: false,
+  },
+  
+  /** 是否给文字设置多彩颜色, 和emoji不兼容 */
+  // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
+  IS_SHOW_COLOR: false,
+  
+  /**
+   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
+   */
   USERS: [
     {
       // 想要发送的人的名字
       name: '主人',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      // 使用其他通道时，请严格按照各个通道的教程进行填写
       id: 'ouTx26gxwDLnUE7ZxAttBmlArzaU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '79lxdkoB5wXIP6i5JIZgAbjUHbN6tcgO3AvF9Kars1Y',
-    },
-  ],
+      // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
+      useTemplateId: 'tGkgO5sGyrfsw7JslHpU9CSy90Qau5-qFSKQHGDBktM	',
+      // 所在省份或城市，也可以不填
+      province: '四川',
+      // 所在城市或县区
+      city: '仪陇',
+    }
+    ],
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '主人', year: '2005', date: '08-21',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '主人', year: '2005', date: '09-24',
+        }
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 相识的日子
+        { keyword: 'meet_day', date: '2023-09-01' }
+      ],
 
+      
+  /**
+   * 【推送完成提醒】 相关，主要用来展示发送是否成功/失败的数据
+   */
 
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  // 使用微信测试号：【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  // 使用其他通道时，请填写 config/template-config.cjs 中【推送完成提醒】模板的id
   CALLBACK_TEMPLATE_ID: 'oES-nuJV2J7ml28haCZLXKBOodpXjZ74E8ESlONsykg',
 
+  // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
     {
+      // 一般都填自己
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      // 使用其他通道时，请严格按照各个通道的教程进行填写
       id: 'ouTx26nZ9MGO02dPwbalbpiglvV4',
     }
   ],
 
-}
+  /**
+   * 信息配置
+   */
+  /** 重要节日相关 */
+
+  /**
+   * 重要节日，修改名字为对应需要显示的名字, date 仅填月日即可, 请严格按照示例填写
+   * type必须填！ 只能 “生日” 和 “节日” 二选一!
+   *
+   * --- 阴历配置开始 ---
+   * 如果日期使用阴历，请在 “生日” 或 "节日“ 前添加 * 符号
+   * --- 阴历配置结束 ---
+   *
+   * 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
+   * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
+   *
+   * --- 是否展示周岁信息开始 ---
+   * isShowAge: true 展示岁数, 仅type为生日生效
+   * isShowAge: false 不展示岁数, 仅type为生日生效
+   * 删除isShowAge属性，也会不展示岁数
+   * --- 是否展示周岁信息结束 ---
+   */
+
 
 module.exports = USER_CONFIG
+
