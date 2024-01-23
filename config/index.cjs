df2d96e87c681eece8c1fefec6f2edf2b2226cce
源动力 <98251238+yuan-power-plus@users.noreply.github.com>
2024-01-23 14:19:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ouTx26gxwDLnUE7ZxAttBmlArzaU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'wwX3rrthN0IHXkST8ObXDXLjop6Bv7GyaHALTzP71bM',
+      useTemplateId: 'x1jw39iPMjEghnPVjPibM2PiI-126GvgvzTI7QXBMGs',
     },
   ],
 
