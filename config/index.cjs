08248f15a7deb6b188ac5bf24ba5f5e66e091f9a
源动力 <98251238+yuan-power-plus@users.noreply.github.com>
2024-06-05 23:09:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -17,7 +17,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ouTx26nZ9MGO02dPwbalbpiglvV4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'NU44dVz-BXTby1BBxfCDMjZqnQRFlFSyKbIruT1KaHc',
+      useTemplateId: 'bL83jrm7xr-CmIQaI0iQHbdF_AA3ERE9jI8NOhwXxxA',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
